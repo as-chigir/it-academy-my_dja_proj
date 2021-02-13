c55ea02b7da32207c2a8d573aa1a5a750604f989
Alexa <as.chigir@gmail.com>
2021-02-13 19:23:13 +0300

    Iteration 5
    Add detalied views

--- dacha/urls.py
+++ dacha/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = 'dacha'
 
 urlpatterns = [
-    path('', views.all_materials, name='all_materials'),
+    path('', views.all_materials_, name='all_materials_'),
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>',
+         views.detailed_material,
+         name='detailed_materials')
 ]
--- dacha/views.py
+++ dacha/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . import models
 
 # Create your views here.
 
 
-def all_materials(request):
-    all_materials = models.Material.objects.all()
+def all_materials_(request):
+    materials = models.Material.objects.all()
     return render(request, 'materials/all_materials.html',
-                  {'materials': all_materials})
+                  {'materials': materials})
+
+
+def detailed_material(request, year, month, day, slug):
+    materials = get_object_or_404(models.Material,
+                                  publish__year=year,
+                                  publish__month=month,
+                                  publish__day=day,
+                                  slug=slug)
+    return render(request, 'materials/detailed_materials.html',
+                  {'materials': materials})
