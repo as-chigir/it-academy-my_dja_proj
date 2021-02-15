c364fbd63e18cbdd3226fd70942f61c9418c5679
Alexa <as.chigir@gmail.com>
2021-02-15 20:34:38 +0300

    Iteration 8
    Custom Login

--- dacha/urls.py
+++ dacha/urls.py
@@ -6,6 +6,7 @@ app_name = 'dacha'
 
 urlpatterns = [
     path('', views.all_materials_, name='all_materials_'),
+    # path('', views.MaterialListView.as_view(), name='all_materials_'),
     path('<int:year>/'
          '<int:month>/'
          '<int:day>/'
@@ -16,4 +17,6 @@ urlpatterns = [
          views.share_material, name='share_material'),
     path('create/',
          views.create_form, name='create_form'),
+    path('login/',
+         views.custom_login, name='login'),
 ]
--- dacha/views.py
+++ dacha/views.py
@@ -4,6 +4,15 @@ from django.core.mail import send_mail
 
 from django.contrib.auth.models import User
 
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+
+from django.contrib.auth.decorators import login_required
+
+from django.http import HttpResponse
+
+from django.views.generic import ListView
+
 from . import constants
 from . import forms
 from . import models
@@ -17,6 +26,13 @@ def all_materials_(request):
                   {"materials": materials})
 
 
+# class MaterialListView(LoginRequiredMixin, ListView):
+#     queryset = models.Material.objects.all()
+#     context_object_name = 'materials'
+#     template_name = "materials/all_materials.html"
+
+
+@login_required
 def detailed_material(request, year, month, day, slug):
     material = get_object_or_404(models.Material,
                                  publish__year=year,
@@ -73,3 +89,25 @@ def create_form(request):
         return render(request,
                       'materials/create.html',
                       {'form': material_form})
+
+
+def custom_login(request):
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                usermane=cd['username'],
+                password=cd['password'],
+            )
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('User was logged init')
+                else:
+                    return HttpResponse('User not active')
+            else:
+                return HttpResponse('bad credentials')
+    else:
+        form = forms.LoginForm()
+        return render(request, 'login.html', {'form': form})
