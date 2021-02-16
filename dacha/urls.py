d7e80d3a67e9d4dcdacb9daf9bb7fc8d5c073c4d
Alexa <as.chigir@gmail.com>
2021-02-16 20:18:45 +0300

    Iteration 10
    password_reset

--- dacha/urls.py
+++ dacha/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import  views as auth_views
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 app_name = 'dacha'
+
+
+class MyHack(auth_views.PasswordResetView):
+    success_url = reverse_lazy('dacha:password_reset_done')
+
 
 urlpatterns = [
     path('', views.all_materials_, name='all_materials_'),
@@ -20,4 +26,18 @@ urlpatterns = [
     # path('login/', views.custom_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('password_reset/', MyHack.as_view(), name='password_reset'),
+
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             success_url=reverse_lazy('dacha:password_reset_complete'),
+         ),
+         name='password_reset_confirm'),
+
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
 ]
