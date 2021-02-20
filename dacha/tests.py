30e6af3b0fcc09c749ed8ef90b8497dac4f03b11
Alexa <as.chigir@gmail.com>
2021-02-20 10:40:20 +0300

    Iteration 18.
    Tests (mocks)

--- dacha/tests.py
+++ dacha/tests.py
@@ -1,8 +1,11 @@
 from django.test import TestCase, Client
 from dacha import models
+from unittest import mock
+
 # Create your tests here.
 
 import ddt
+from . import constants
 
 
 @ddt.ddt
@@ -52,3 +55,53 @@ class MaterialTestCase(TestCase):
         )
         obj = models.Material.objects.get()
         self.assertEqual(obj.slug, expected_slug)
+
+    def test_send_mail(self):
+        material = models.Material(slug='slug', author=self.user,
+                                   body='body')
+        material.save()
+
+        with mock.patch('dacha.views.send_mail') as mail_mock:
+            self.client.post(
+                '/' + str(material.id) + '/share/',
+                {'name': 'name',
+                 'to_email': 'e@e.com',
+                 'comment': 'comment'},
+            )
+
+        mail_mock.assert_called_once()
+
+    @mock.patch('dacha.views.send_mail')
+    def test_send_mail_args(self, mail_mock):
+        material = models.Material(slug='slug', author=self.user,
+                                   body='body')
+        material.save()
+
+        self.client.post(
+            '/' + str(material.id) + '/share/',
+            {'name': 'name',
+             'to_email': 'e@e.com',
+             'comment': 'comment'},
+            )
+
+        body = constants.SHARE_EMAIL_BODY
+        uri = "http://testserver/{y}/{m}/{d}/{slug}/".format(
+            y=material.publish.year,
+            m=material.publish.month,
+            d=material.publish.day,
+            slug=material.slug
+        )
+        body = body.format(title='', uri=uri, comment='comment')
+        mail_mock.assert_called_with(
+            'Dear name. Please take a look at next material ""',
+            body,
+            'admin@my.com',
+            ['e@e.com']
+        )
+
+    def test_message(self):
+        self.assertEqual(
+            constants.SHARE_EMAIL_BODY,
+            ('{title} at {uri} \n\nAdmin asks you to take a look at this'
+             ' material with next comment:\n\nComment: {comment}'),
+        )
